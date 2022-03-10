a916edca433d3fb5483806663b678a6f573f772d
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-03-09 23:42:28 -0300

    Projeto inicial de Partidas
    Primeiros passos

--- app/src/main/java/io/sportheca/simulator/MainActivity.kt
+++ app/src/main/java/io/sportheca/simulator/MainActivity.kt
@@ -2,10 +2,20 @@ package io.sportheca.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.sportheca.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvHello.text = "Hello, Wellington"
+
+
     }
 }
